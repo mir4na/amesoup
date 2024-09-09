905a978f77d87f69ef994daede2bc6d50e36ca0b
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-09-09 19:41:55 +0700

    adding models: name, price, etc + adjust main.html

--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-class MoodEntry(models.Model):
-    mood = models.CharField(max_length=255)
-    time = models.DateField(auto_now_add=True)
-    feelings = models.TextField()
-    mood_intensity = models.IntegerField()
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
+    def __str__(self):
+        return self.name
--- main/views.py
+++ main/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306208855',
-        'name': 'Muhammad Afwan Hafizh',
-        'class': 'PBP F'
+        'app_name': 'Amesoup',
+        'user_name': 'Muhammad Afwan Hafizh',
+        'class_name': 'PBP-F'
     }
 
     return render(request, "main.html", context)
