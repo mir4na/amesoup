41e8d10d3f7068939a48b338951dc6239104ea3f
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-09-10 14:07:20 +0700

    adding design also another page + setup route

--- main/views.py
+++ main/views.py
@@ -8,3 +8,17 @@ def show_main(request):
     }
 
     return render(request, "main.html", context)
+
+def account_page(request):
+    context = {
+        'app_name': 'Amesoup',
+        'user_name': 'Muhammad Afwan Hafizh',
+        'class_name': 'PBP-F'
+    }
+    return render(request, 'account.html', context)
+
+def products_page(request):
+    return render(request, 'products.html')
+
+def cart_page(request):
+    return render(request, 'cart.html')
